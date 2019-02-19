d031ef8dba4756f90f0e26da2aee2de82e6bbeee
Dan Vernon <danvernon@gmail.com>
2019-02-19 15:58:45 +0000

    rails task manager - to-do list w/ CRUD functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'tasks' => 'tasks#index'
+  get 'tasks/new' => 'tasks#new'
+  post 'tasks' => 'tasks#create'
+  get 'tasks/:id' => 'tasks#show', as: 'task'
+  get 'tasks/:id/edit' => 'tasks#edit', as: 'edit'
+  patch 'tasks/:id' => 'tasks#update'
+  get 'tasks/:id/destroy' => 'tasks#destroy', as: 'destroy'
+  get 'tasks/:id/complete' => 'tasks#complete', as: 'complete'
 end
